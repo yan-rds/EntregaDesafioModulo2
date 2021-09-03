1c38de79ae86a40003a5b292d7f577292fca242f
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-03 10:36:46 -0300

    Primeira parte do console

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        // Variáveis
+        int escolhaMenu;
+
+
+        // Primeira parte do console
+        System.out.println("Sistema de gerenciamento de funcionários");
+        System.out.println();
+        System.out.println("Utilize os números indicados para navegar pelo menu");
+        System.out.println("1 - Adicionar funcionários");
+        System.out.println("2 - Remover funcionários");
+        System.out.println("3 - Exibir a lista de funcionários");
+        escolhaMenu = leitor.nextInt();
     }
 }
